462b1534c86a1bf284ba3a433963d15052e8b0b0
T0nd0Tara <5632815@gmail.com>
2025-01-09 10:06:19 +0200

    add odin lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -6,7 +6,7 @@ local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "ts_ls", "clangd", "pyright", "lua_ls", "rust-analyzer" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "pyright", "lua_ls", "rust-analyzer", "ols" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
