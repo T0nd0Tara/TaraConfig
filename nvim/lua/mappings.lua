67208d89942cdfb86cc629fa639f37ed7e4b36b1
T0nd0Tara <5632815@gmail.com>
2025-01-26 10:04:23 +0200

    fix trouble mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -67,7 +67,7 @@ map("n", "<leader>gh", "<cmd> DiffviewFileHistory % <CR>", { desc = "Git diff vi
 map("n", "<leader>lr", "<cmd> LspRestart <CR>", { desc = "Lsp Restart" })
 
 map("n", "<leader>lx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics" })
-map("n", "<leader>lc", "<cmd>Current Diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics (Trouble)" })
+map("n", "<leader>lc", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics" })
 map("n", "<leader>ls", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols" })
 map("n", "<leader>ld", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "LSP Definitions / references / ..." })
 map("n", "<leader>lL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List" })
