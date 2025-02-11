6efea3bb74adf8f3f11db27d5e4beb25a7510198
T0nd0Tara <5632815@gmail.com>
2025-02-11 09:07:04 +0200

    add get_visual function

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,13 +1,18 @@
 local M = {}
 
-function M.get_formatted_visual()
+function M.get_visual()
   local saved_reg = vim.fn.getreg('a')
   local saved_regtype = vim.fn.getregtype('a')
 
   vim.cmd('normal! "ay')
   local text = vim.fn.getreg('a')
   vim.fn.setreg('a', saved_reg, saved_regtype)
-  return text:gsub('\n', '\\n'):gsub('([%(%).%%%+%-%*%?%[%]%^%$%\\%{%}%|])', '\\%1'):gsub('\\\\n', '\\n')
+
+  return text
+end
+
+function M.get_formatted_visual()
+  return M.get_visual():gsub('\n', '\\n'):gsub('([%(%).%%%+%-%*%?%[%]%^%$%\\%{%}%|])', '\\%1'):gsub('\\\\n', '\\n')
 end
 
 
