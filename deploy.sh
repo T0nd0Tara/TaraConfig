1669a7aff22172dcc533d426458adf442b740a9c
T0nd0Tara <5632815@gmail.com>
2025-03-31 20:32:59 +0300

    make deploy script great again

--- deploy.sh
+++ deploy.sh
@@ -9,23 +9,25 @@ SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 symlinkFile() {
     from="$SCRIPT_DIR/$1"
     filename=$(basename $1)
-    destination="$HOME/$2/$filename"
+    destination="$2/$3"
 
     mkdir -p $(dirname "$destination")
 
-    if [ -L "$destination" ]; then
-        echo "[WARNING] $from already symlinked"
-        return
-    fi
-
-    if [ -e "$destination" ]; then
+    if [ -e "$destination" ] && [ ! -L "$destination" ]; then
         echo "[ERROR] $destination exists but it's not a symlink. Please fix that manually"
         exit 1
     fi
 
-    ln -s "$from" "$destination"
+    ln -f -s "$from" "$destination"
     echo "[OK] $from -> $destination"
 }
+
+getRowArguement() { 
+  arg=$(echo "$1" | cut -d \| -f "$2");
+  if [ "$arg" != '~' ]; then
+    echo $arg
+  fi
+};
 
 deployManifest() {
     for row in $(cat $SCRIPT_DIR/$1); do
@@ -33,13 +35,19 @@ deployManifest() {
             continue
         fi
 
-        filename=$(echo $row | cut -d \| -f 1)
-        operation=$(echo $row | cut -d \| -f 2)
-        destination=$(echo $row | cut -d \| -f 3)
+        filename=$(getRowArguement "$row" 1)
+        operation=$(getRowArguement "$row" 2)
+
+        destination=$(getRowArguement "$row" 3)
+        destination=${destination:-~}
+
+        rename=$(getRowArguement "$row" 4)
+        rename=${rename:-$(basename $filename)}
+        echo "[INFO]: filename: $filename, op: $operation, dest: $destination, rename: $rename"
 
         case $operation in
             symlink)
-                symlinkFile $filename $destination
+                symlinkFile $filename $destination $rename
                 ;;
 
             *)
