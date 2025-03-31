2a7a0be63f583fd2fb64259edc7738ff868788b5
T0nd0Tara <5632815@gmail.com>
2025-03-31 22:05:24 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,63 +4,62 @@
 
 set -e
 
-SCRIPT_DIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "$BASH_SOURCE[0]")" && pwd)"
 
 symlinkFile() {
-    from="$SCRIPT_DIR/$1"
-    filename=$(basename $1)
-    destination="$2/$3"
+  from="$SCRIPT_DIR/$1"
+  filename=$(basename $1)
+  destination="$HOME/$2/${3:-$filename}"
 
-    mkdir -p $(dirname "$destination")
+  mkdir -p $(dirname "$destination")
 
-    if [ -e "$destination" ] && [ ! -L "$destination" ]; then
-        echo "[ERROR] $destination exists but it's not a symlink. Please fix that manually"
-        exit 1
-    fi
+  if [ -e "$destination" ] && [ ! -L "$destination" ]; then
+    echo "[ERROR] $destination exists but it's not a symlink. Please fix that manually"
+    exit 1
+  fi
 
-    ln -f -s "$from" "$destination"
-    echo "[OK] $from -> $destination"
+  if [ -L "$destination" ]; then 
+    rm "$destination"
+  fi
+
+  ln -f -s "$from" "$destination"
+  echo "[OK] $from -> $destination"
 }
 
-getRowArguement() { 
-  arg=$(echo "$1" | cut -d \| -f "$2");
-  if [ "$arg" != '~' ]; then
-    echo $arg
-  fi
-};
+getRowArguement() {
+  echo "$1" | cut -d \| -f "$2"
+}
 
 deployManifest() {
-    for row in $(cat $SCRIPT_DIR/$1); do
-        if [[ "$row" =~ ^#.* ]]; then
-            continue
-        fi
+  for row in $(cat $SCRIPT_DIR/$1); do
+    if [[ "$row" =~ ^#.* ]]; then
+      continue
+    fi
 
-        filename=$(getRowArguement "$row" 1)
-        operation=$(getRowArguement "$row" 2)
+    filename=$(getRowArguement "$row" 1)
+    operation=$(getRowArguement "$row" 2)
+    destination=$(getRowArguement "$row" 3)
 
-        destination=$(getRowArguement "$row" 3)
-        destination=${destination:-~}
+    rename=$(getRowArguement "$row" 4)
+    rename=${rename:-$(basename $filename)}
+    echo "[INFO]: filename: $filename, op: $operation, dest: $destination, rename: $rename"
 
-        rename=$(getRowArguement "$row" 4)
-        rename=${rename:-$(basename $filename)}
-        echo "[INFO]: filename: $filename, op: $operation, dest: $destination, rename: $rename"
+    case $operation in
+    symlink)
+      symlinkFile "$filename" "$destination" "$rename"
+      ;;
 
-        case $operation in
-            symlink)
-                symlinkFile $filename $destination $rename
-                ;;
-
-            *)
-                echo "[WARNING] Unknown operation $operation. Skipping..."
-                ;;
-        esac
-    done
+    *)
+      echo "[WARNING] Unknown operation $operation. Skipping..."
+      ;;
+    esac
+  done
 }
 
 if [ -z "$@" ]; then
-    echo "Usage: $0 <MANIFEST>"
-    echo "ERROR: no MANIFEST file is provided"
-    exit 1
+  echo "Usage: $0 <MANIFEST>"
+  echo "ERROR: no MANIFEST file is provided"
+  exit 1
 fi
 
 deployManifest $1
