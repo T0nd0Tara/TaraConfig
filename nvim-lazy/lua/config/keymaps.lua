64583ff046ba54a1da76552ff703fa2780b4df3a
T0nd0Tara <5632815@gmail.com>
2025-04-13 17:09:42 +0300

    forgot nvim tree is just better than snacks for most stuff

--- nvim-lazy/lua/config/keymaps.lua
+++ nvim-lazy/lua/config/keymaps.lua
@@ -16,7 +16,7 @@ end
 pcall(vim.keymap.del, { "n", "v" }, "s")
 map({ "n", "v" }, "L", "$")
 map({ "n", "v" }, "H", "^")
-local useNvimTree = false
+local useNvimTree = true
 
 if useNvimTree then
   map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
