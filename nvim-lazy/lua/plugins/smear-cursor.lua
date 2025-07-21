453c7e17f06ae42102802b7c82a8dbab040ed03a
T0nd0Tara <5632815@gmail.com>
2025-07-21 10:28:51 +0300

    remove custom bad config for smear-cursor

--- nvim-lazy/lua/plugins/smear-cursor.lua
+++ nvim-lazy/lua/plugins/smear-cursor.lua
@@ -23,11 +23,11 @@ return {
     -- Smears will blend better on all backgrounds.
     legacy_computing_symbols_support = false,
 
-                                   -- Default  Range
-    stiffness = 0.6,               -- 0.6      [0, 1]
-    trailing_stiffness = 0.3,      -- 0.3      [0, 1]
-    trailing_exponent = 0.1,       -- 0.1      >= 0
-    distance_stop_animating = 0.1, -- 0.1      > 0
-    hide_target_hack = true,       -- true     boolean
+    --                                -- Default  Range
+    -- stiffness = 0.6,               -- 0.6      [0, 1]
+    -- trailing_stiffness = 0.3,      -- 0.3      [0, 1]
+    -- trailing_exponent = 0.1,       -- 0.1      >= 0
+    -- distance_stop_animating = 0.1, -- 0.1      > 0
+    -- hide_target_hack = true,       -- true     boolean
   },
 }
